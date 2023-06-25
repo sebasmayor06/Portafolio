926c9d86a2f537bb2577d7dc3d629667d3e310e2
sebasmayor06 <sebda06@gmail.com>
2023-06-25 15:50:22 -0500

    Page proyectos terminada

--- src/app/components/Contactame/page.jsx
+++ src/app/components/Contactame/page.jsx
@@ -22,22 +22,22 @@ export default function page() {
 
 
   return (
-    <div className='flex flex-col  gap-24'>
+    <div className='flex flex-col min-h-screen gap-24'>
 
       <h1  className='animate-fade-down animate-once animate-duration-1000 animate-delay-500 animate-ease-out animate-normal animate-fill-backwards flex justify-center font-bold text-5xl text-gray-400 sm:text-7xl tracking-wide md:tracking-widest m-4'> CONTACTAME <span className='text-red-600'>.</span></h1>
-    <div className='flex flex-col log:flex log:flex-row justify-around items-center '>
-<div>
-  <Image src={img} width={300} height={300} ></Image>
+    <div className='flex flex-col lg:flex lg:flex-row justify-around items-center '>
+<div >
+ 
 </div>
 <div>
 
     <form className='flex flex-col  relative border-2 border-solid border-red-600 rounded-lg p-4' ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input className='w-[250px] lg:w-[600px] mb-10' type="text" name="user_name" />
-      <label>Email</label>
-      <input className='w-[250px] lg:w-[600px] mb-10' type="email" name="user_email" />
-      <label>Message</label>
-      <textarea className='w-[250px] lg:w-[600px] mb-10' name="message" />
+      <label className='font-bold text-gray-400' >NOMBRE</label>
+      <input className='w-[250px] font-bold text-black lg:w-[600px] mb-10' type="text" name="user_name" />
+      <label className='font-bold text-gray-400'>EMAIL</label>
+      <input className='w-[250px] font-bold text-black lg:w-[600px] mb-10' type="email" name="user_email" />
+      <label className='font-bold text-gray-400'>MENSAJE</label>
+      <textarea className='w-[250px] font-bold text-black lg:w-[600px] mb-10' name="message" />
       <button className='text-gray-400 absolute bottom-4 right-1/2 border-2 border-solid border-gray-600 w-20 bg-red-500 bg-opacity-50' type='submit'> ENVIAR </button>
     </form>
 </div>
